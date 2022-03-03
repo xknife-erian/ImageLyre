ee2be969ef827e11b0d758c51f1376793f07779e
lukan-home <lukan@xknife.net>
2022-03-03 23:08:44 +0800

    develop 菜单和工具栏整得有一些样子了。先这样。其他的做一个功能再放一个。开始实现新建图像和打开图像。

---
+++ src/ImageLaka.ViewModels/WorkbenchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace ImageLaka.ViewModels;
+
+public class WorkbenchViewModel : ObservableRecipient
+{
+    public ICommand NewImage { get; set; }
+    public ICommand OpenImage { get; set; }
+}
