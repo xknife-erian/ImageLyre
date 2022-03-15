bafce655ba5e38f66842b56f0fe20a43b8abb080
lukan.xknife.net <lukan@xknife.net>
2022-03-15 17:19:47 +0800

    .

--- src/ImageLaka.Base/Managers/ConsoleManager.cs
+++ src/ImageLaka.Base/Managers/ConsoleManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NationalInstruments.Tdms;
 
 namespace ImageLaka.Managers
 {
@@ -10,7 +11,7 @@ namespace ImageLaka.Managers
     {
         public void Initianize()
         {
-
+            Reader reader;
         }
     }
 }
