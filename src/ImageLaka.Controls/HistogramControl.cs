9be26ae487e67d08c10bec66e7f0a097c108f319
lukan-home <lukan@xknife.net>
2022-03-18 23:52:33 +0800

    fix.

--- src/ImageLaka.Controls/HistogramControl.cs
+++ src/ImageLaka.Controls/HistogramControl.cs
@@ -22,6 +22,7 @@ public sealed class HistogramControl : Canvas
 
     public HistogramControl()
     {
+        //为本控制定义一个唯一的绘图器
         _drawingVisual = new DrawingVisual();
         AddVisualChild(_drawingVisual);
     }
@@ -62,32 +63,38 @@ public sealed class HistogramControl : Canvas
 
     #endregion
 
+    /// <summary>
+    /// 实时绘制直方图
+    /// </summary>
     private void DrawHistogram(DrawingContext dc)
     {
+        //画背景色，相当于清空整个画布
         dc.DrawRectangle(Background, null, new Rect(0, 0, ActualWidth, ActualHeight));
-        var max = DataSource.Max();
+        var max = DataSource.Max();//得到最大的值，即柱状图(直方图实际上就是柱状图)最高的柱。
         var count = DataSource.Length;
         if (ActualWidth <= 0 || ActualHeight <= 0)
             return;
         var width = ActualWidth;
         var height = ActualHeight;
-        var th = 12;
-        var ath = height / 6;
-        if (ath < th)
-            th = (int) ath;
-        var lb = new LinearGradientBrush(Colors.Black, Colors.White, 0);
-        dc.DrawRectangle(lb, null, new Rect(0, height - th, width, th));
-        height -= th;
-        var bl = height / max; //高度比率
-        var w = width / count;
-        var b = new SolidColorBrush(Colors.DarkSlateBlue);
+        var grayAxle = 12;//x轴，用黑到白的渐变表示
+        var minGrayAxle = height / 6;//当直方图高度很小时，x轴至少占整图的1/6高度
+        if (minGrayAxle < grayAxle)
+            grayAxle = (int) minGrayAxle;
+        //x轴的渐变笔刷
+        var linearGradientBrush = new LinearGradientBrush(Colors.Black, Colors.White, 0);
+        //画x轴
+        dc.DrawRectangle(linearGradientBrush, null, new Rect(0, height - grayAxle, width, grayAxle));
+        height -= grayAxle;//直方图的高度
+        var bl = height / max; //根据最大值计算出y轴高度的换算比率
+        var w = width / count; //每个值的宽度
+        var b = new SolidColorBrush(Colors.DarkSlateBlue);//画柱子的笔刷
         for (var i = 0; i < count; i++)
         {
             var rh = DataSource[i] * bl;
             var rect = new Rect(w * i, height - rh, w, rh);
-            dc.DrawRectangle(b, null, rect);
+            dc.DrawRectangle(b, null, rect);//画柱子
         }
-
+        //画边界(border)
         dc.DrawRectangle(null, new Pen(Brushes.Black, 0.1), new Rect(0, 0, ActualWidth - 1, ActualHeight - 1));
     }
 
@@ -95,6 +102,9 @@ public sealed class HistogramControl : Canvas
 
     #region 依赖属性：数据源
 
+    /// <summary>
+    /// 直方图的数据源。
+    /// </summary>
     [Category("数据源")]
     public int[] DataSource
     {
@@ -126,6 +136,9 @@ public sealed class HistogramControl : Canvas
         return baseValue;
     }
 
+    /// <summary>
+    /// 当有源或源有变化时，重绘画布。
+    /// </summary>
     private void OnHasSource()
     {
         using var dc = _drawingVisual.RenderOpen();
