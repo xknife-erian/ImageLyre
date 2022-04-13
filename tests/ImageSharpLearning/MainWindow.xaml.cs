5d52678825eac6905739e5c8d1bb0797c28d5422
lukan.xknife.net <lukan@xknife.net>
2022-04-13 15:29:22 +0800

    .

--- tests/ImageSharpLearning/MainWindow.xaml.cs
+++ tests/ImageSharpLearning/MainWindow.xaml.cs
@@ -15,8 +15,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ImageLad.Views.Utils;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.ColorSpaces.Conversion;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using Image = SixLabors.ImageSharp.Image;
 
 namespace ImageSharpLearning
@@ -28,24 +31,41 @@ namespace ImageSharpLearning
     {
         public MainWindow()
         {
-            var image = Image.Load(File.Open("06.jpg", FileMode.Open));
-            var vm = new MainWindowViewModel();
-            vm.Image = new ImageSharpSource<Rgb24>((Image<Rgb24>) image);
-
-            DataContext = vm;
-
+            DataContext = new MainWindowViewModel("06.jpg");
             InitializeComponent();
         }
     }
 
     public class MainWindowViewModel : ObservableRecipient
     {
+        private string _file;
         private ImageSharpSource<Rgb24> _image;
+
+        public MainWindowViewModel(string file)
+        {
+            _file = file;
+            Image = new ImageSharpSource<Rgb24>(GetImage());
+        }
+
+        private Image<Rgb24> GetImage()
+        {
+            var stream = new MemoryStream(File.ReadAllBytes(_file));
+            var image = SixLabors.ImageSharp.Image.Load(stream);
+            return (Image<Rgb24>)image;
+        }
 
         public ImageSharpSource<Rgb24> Image
         {
             get => _image;
             set => SetProperty(ref _image, value);
         }
+
+        public ICommand RGB2GrayCommand => new RelayCommand(() =>
+        {
+            var img = GetImage();
+            var converter = new ColorSpaceConverter(new ColorSpaceConverterOptions());
+
+            Image = new ImageSharpSource<Rgb24>(img);
+        });
     }
 }
