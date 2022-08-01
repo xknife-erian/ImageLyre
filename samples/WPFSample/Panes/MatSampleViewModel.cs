22979c3ae7c1ecd0fb5be14f0b9bf932576f6ced
lukan.xknife.net <lukan@xknife.net>
2022-08-01 16:21:25 +0800

    开始试验Mat结构

--- samples/WPFSample/Panes/MatSampleViewModel.cs
+++ samples/WPFSample/Panes/MatSampleViewModel.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using OpenCvSharp;
 
-namespace WPFSample.Panes
+namespace WPFSample.Panes;
+
+public class MatSampleViewModel : ObservableRecipient
 {
-    public class MatSampleViewModel
+    private Mat? _bitmap;
+    public int ImageWidth { get; } = 400;
+    public int ImageHeight { get; } = 400;
+
+    public Mat? Bitmap
     {
+        get => _bitmap;
+        set => SetProperty(ref _bitmap, value);
     }
+
+    public ICommand BuildRandomImageCommand => new RelayCommand(() =>
+    {
+        Bitmap = new Mat(new[] {1}, MatType.CV_8SC3, Scalar.Red);
+    });
 }
