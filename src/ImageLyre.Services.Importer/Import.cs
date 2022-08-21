43927600a36a8451a60e091eabf3fce0638aff6a
lukan-home <lukan@xknife.net>
2022-08-21 21:51:00 +0800

    项目改名后，修正c语言导入细节

--- src/ImageLyre.Services.Importer/Import.cs
+++ src/ImageLyre.Services.Importer/Import.cs
@@ -4,7 +4,7 @@ namespace ImageLyre.Services.Importer
 {
     public static class Import
     {
-        [DllImport("Wrapper\\libimglyric.dll", EntryPoint = "calc")]
+        [DllImport("Wrapper\\libimglyre.dll", EntryPoint = "calc")]
         public static extern int Calc(int a1, int a2, int b1, int b2);
     }
 }
--- tests/UTest.ServicesTest/Importer/ImportTests.cs
+++ tests/UTest.ServicesTest/Importer/ImportTests.cs
@@ -9,7 +9,7 @@ public class ImportTests
     [Fact()]
     public void BaseTest()
     {
-        var result = Import.Calc(1, 2, 3, 4);
-        result.Should().Be((1 + 2) * (3 + 4));
+        var result = Import.Calc(11, 22, 33, 44);
+        result.Should().Be((11 + 22) * (33 + 44));
     }
 }
